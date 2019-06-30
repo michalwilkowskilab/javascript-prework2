82a45502340ead54f575a82ca0bcb0808c15260d
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-06-30 19:01:00 +0200

    Connect buttons with game

--- js/script.js
+++ js/script.js
@@ -1,16 +1,62 @@
-var argButtonName, buttonPaper, buttonRock, buttonScissors, kamien, nozyce, papier, x;
+var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest, x,
+argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
+
+buttonScissors = document.getElementById('button-scissors');
+buttonPaper = document.getElementById('button-paper');
+buttonRock = document.getElementById('button-rock');
+/**
+ * Describe this function...
+ */
+function buttonClicked(argButtonName) {
+  console.log(argButtonName + ' został kliknięty');
+  printMessage(argButtonName + ' został kliknięty');
+}
 
 /**
  * Describe this function...
  */
-function buttonClicked(argButtonName, kamien, papier, nozyce) {
-  console.log(kamien + papier + nozyce + ' został kliknięty');
-  printMessage(kamien + papier + nozyce + ' został kliknięty');
+function getMoveName(argMoveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+    return 'kamień';
+  }
 }
-buttonScissors = document.getElementById('button-scissors');
-buttonPaper = document.getElementById('button-paper');
-buttonRock = document.getElementById('button-rock');
 
-buttonRock.addEventListener('click', function(){ buttonClicked('', 'Guzik kamień', '', ''); });
-buttonPaper.addEventListener('click', function(){ buttonClicked('', '', 'Guzik papier', ''); });
-buttonScissors.addEventListener('click', function(){ buttonClicked('', '', '', 'Guzik nożyce'); });
+playerMove = argButtonName;
+console.log('ruch gracza to: ' + playerMove);
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log('ruch komputera to: ' + computerMove);
+displayResult(playerMove, computerMove);
+
+/**
+ * Describe this function...
+ */
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == argComputerMove) {
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrywasz :(');
+  }
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+}
+
+
+buttonRock.addEventListener('click', function(){ buttonClicked('Guzik kamień'); });
+buttonPaper.addEventListener('click', function(){ buttonClicked('Guzik papier'); });
+buttonScissors.addEventListener('click', function(){ buttonClicked('Guzik nożyce'); });
